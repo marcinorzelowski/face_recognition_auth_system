72d9b612ffe46e848922e04432fe0ee51b51bdad
orzelke <marcin.orzelowski.stud@pw.edu.pl>
2022-04-26 23:42:57 +0200

    Add proper request to python backend.

--- jwt_auth_service/src/main/java/com/orzelowski/jwt_auth_service/service/client/FaceRecognitionClientService.java
+++ jwt_auth_service/src/main/java/com/orzelowski/jwt_auth_service/service/client/FaceRecognitionClientService.java
@@ -1,15 +1,11 @@
 package com.orzelowski.jwt_auth_service.service.client;
 
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Service
 public class FaceRecognitionClientService {
@@ -23,31 +19,17 @@ public class FaceRecognitionClientService {
         this.faceRecognitionClient = WebClient.create("http://localhost:5000");
     }
 
-
-    public void authenticate(MultipartFile image) {
+    public Boolean authenticate(MultipartFile image, String username) {
         MultipartBodyBuilder builder = new MultipartBodyBuilder();
         builder.part("file", image.getResource());
-        this.faceRecognitionClient.post()
+        builder.part("username", username);
+        return this.faceRecognitionClient.post()
                 .uri("/user")
                 .body(BodyInserters.fromMultipartData(builder.build()))
-                .exchangeToMono(clientResponse -> {
-                    if(clientResponse.statusCode().equals(HttpStatus.OK)) {
-                        System.out.println("Positivoo");
-                        return clientResponse.bodyToMono(Boolean.class).thenReturn(clientResponse.statusCode());
-                    } else {
-                        throw new ServiceException("Error!");
-                    }
-                });
-
-    }
-
-    public void test() {
-        Mono<String> test = this.faceRecognitionClient.get()
-                .uri("/user")
                 .retrieve()
-                .bodyToMono(String.class)
-                        ;
-
-        test.subscribe(System.out::println);
+                .bodyToMono(Boolean.class)
+                .block();
     }
+
 }
+
